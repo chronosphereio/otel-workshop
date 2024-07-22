a1ea820e772bd2a7f4865583b80ea7ba26cbf6fd
linchar <linchar@gmail.com>
2024-07-22 16:58:42 -0600

    separate otel configureation to its own source file

--- lab2-instrumentation/pricing/src/main.rs
+++ lab2-instrumentation/pricing/src/main.rs
@@ -2,18 +2,16 @@ use actix_web::{web, App, HttpServer, Responder, Result};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 use tokio::task;
-use std::env;
+
 use opentelemetry::{
     global,
     trace::{TraceContextExt, Tracer},
-    Context, KeyValue,
+    Context,
 };
 // use opentelemetry_http::{Bytes, HeaderExtractor};
 use actix_web_opentelemetry::RequestTracing;
-use opentelemetry_otlp::{TonicExporterBuilder, WithExportConfig};
-use opentelemetry_sdk::{
-    propagation::TraceContextPropagator, runtime::TokioCurrentThread, trace, Resource,
-};
+
+mod telemetry;
 
 #[derive(Serialize, Debug)]
 pub struct PriceResult {
@@ -54,29 +52,13 @@ async fn get_price(query: web::Query<PriceQuery>) -> Result<impl Responder> {
     let price_result = task::spawn_blocking(move || get_price_from_db(parent_cx, id))
         .await
         .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
+
     Ok(web::Json(price_result))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Start a new OTLP trace pipeline
-    global::set_text_map_propagator(TraceContextPropagator::new());
-
-    let service_name_resource = Resource::new(vec![KeyValue::new(
-        opentelemetry_semantic_conventions::resource::SERVICE_NAME,
-        "pricing_rust",
-    )]);
-
-    let _tracer: trace::Tracer = opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_exporter(TonicExporterBuilder::default().with_endpoint(format!(
-            "{}",
-            env::var("OTEL_EXPORTER_OTLP_ENDPOINT")
-                .unwrap_or_else(|_| "http://localhost:4317".to_string())
-        )))
-        .with_trace_config(trace::Config::default().with_resource(service_name_resource))
-        .install_batch(TokioCurrentThread)
-        .expect("pipeline install error");
+    telemetry::init_tracer();
 
     HttpServer::new(|| {
         App::new()
