e521e9ff39c5b71bbc374aedda086a99b3f367d0
linchar <linchar@gmail.com>
2024-07-22 12:55:08 -0600

    add additional comments for operator deployment script

--- lab1-collection/1.2-kubernetes/operator_and_collector.sh
+++ lab1-collection/1.2-kubernetes/operator_and_collector.sh
@@ -14,11 +14,16 @@ elif [[ -z ${CHRONOSPHERE_API_TOKEN+x} ]]; then
 fi
 set -x #echo on
 cd "$(dirname "$0")"
+# For the API server to communicate with the webhook component, the webhook requires a TLS certificate that 
+# the API server is configured to trust. The easiest and default method is to install the cert-manager.
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.0/cert-manager.yaml
+# Wait for all cert-manager resource to be up and running
 sleep 90
 kubectl create namespace collectors
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo update open-telemetry 
+# Use an automatically generated self-signed TLS certificate by setting admissionWebhooks.certManager.enabled
+# to false and admissionWebhooks.autoGenerateCert to true (default)
 helm upgrade --install otel-operator open-telemetry/opentelemetry-operator  \
   --set admissionWebhooks.certManager.enabled=false \
   --set manager.collectorImage.repository="otel/opentelemetry-collector-contrib" \
