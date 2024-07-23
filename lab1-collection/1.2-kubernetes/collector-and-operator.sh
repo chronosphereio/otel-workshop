15f79e009c9755d5fb86c8e132a30877ad03d84f
linchar <linchar@gmail.com>
2024-07-22 19:20:50 -0600

    adjust collector and operator order, remove unnecessary scrape configuration

--- lab1-collection/1.2-kubernetes/collector-and-operator.sh
+++ lab1-collection/1.2-kubernetes/collector-and-operator.sh
@@ -14,12 +14,21 @@ elif [[ -z ${CHRONOSPHERE_API_TOKEN+x} ]]; then
 fi
 set -x #echo on
 cd "$(dirname "$0")"
+kubectl create namespace collectors
+# otel-collector deployed using helm is not managed by otel operator
+# to deploy an otel-collector managed by the operator, deploy a kind of "OpenTelemetryCollector"
+kubectl -n collectors create secret generic chronosphere-secret --from-literal=api-token="$CHRONOSPHERE_API_TOKEN" --from-literal=org="$CHRONOSPHERE_ORG_NAME"
+helm upgrade --install otel-collector open-telemetry/opentelemetry-collector \
+  --values otel-collector-values.yaml \
+  --namespace collectors \
+  --version 0.90.1
+sleep 10
+kubectl -n collectors get pods
 # For the API server to communicate with the webhook component, the webhook requires a TLS certificate that 
 # the API server is configured to trust. The easiest and default method is to install the cert-manager.
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.0/cert-manager.yaml
 # Wait for all cert-manager resource to be up and running
 sleep 90
-kubectl create namespace collectors
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo update open-telemetry 
 # Use an automatically generated self-signed TLS certificate by setting admissionWebhooks.certManager.enabled
@@ -30,14 +39,5 @@ helm upgrade --install otel-operator open-telemetry/opentelemetry-operator  \
   --set manager.extraArgs={"--enable-go-instrumentation"} \
   --namespace collectors \
   --version 0.63.0
-kubectl -n collectors get pods
 sleep 5
-# otel-collector deployed using helm is not managed by otel operator
-# to deploy an otel-collector managed by the operator, deploy a kind of "OpenTelemetryCollector"
-kubectl -n collectors create secret generic chronosphere-secret --from-literal=api-token="$CHRONOSPHERE_API_TOKEN" --from-literal=org="$CHRONOSPHERE_ORG_NAME"
-helm upgrade --install otel-collector open-telemetry/opentelemetry-collector \
-  --values otel-collector-values.yaml \
-  --namespace collectors \
-  --version 0.90.1
-sleep 10
 kubectl -n collectors get all
